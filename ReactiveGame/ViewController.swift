7bef6aedc805aed38b53f9251f13894bffaa71d1
Aubrey Goodman <aubrey.goodman@gmail.com>
2016-09-23 18:12:46 -0700

    added more comments

--- ReactiveGame/ViewController.swift
+++ ReactiveGame/ViewController.swift
@@ -28,23 +28,57 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    //////////////////////////
+    // Timer
+    //
+    // the timer is the core of the physics engine. without a notion of time, 
+    // it's very difficult to model physics. in this case, we tick on a constant
+    // interval and derive all properties from the tick stream
+    
     let interval : Double = 0.1
     let timer : Observable<Int8> = Observable.interval(interval, scheduler: MainScheduler.instance)
+
+    //
+    //////////////////////////
+    
     
     //////////////////////////
-    // X Axis Begin
+    // Common practice
+    //
+    // for each orthogonal axis we model, we assume an initial value and 
+    // iterate forward in time, computing a new "current" value for each tracked item
+    //
+    // we rely on the natural mathematics to compute the position and velocity of tracked items
+    //
+    // in order to include external effects, we introduce multiple observables.
+    // specifically, we introduce a xKick variable to model impulse response. xKick is used
+    // to enable the simulation to consider user input. in this simplified example, we model
+    // external effects in velocity space. typically, this is done in acceleration or jerk space.
+    //
+    //////////////////////////
+
     
+
+    //////////////////////////
+    // X Axis Begin
+    //
     
     // initial x-axis velocity
     let u0 : Double = 0
     
     // u is derived from timer
     let uBase = timer
+      // tick is an integer counter that increases.
+      // we are ignoring it and using the interval instead
       .map { (tick: Int8) in interval }
+      // pattern demands consideration of initial state,
+      // so we inject u0 into the scan seed.
+      // gravity only acts in y-axis, so we don't need to include it in the scan
       .scan(u0) { (total, value) in total }
-        
+
+    // combine the base and kick streams into u
     Observable.combineLatest(uBase, uKick.asObservable()) { $0 }
-      // bind the result to a unique variable.
+      // store the result to a unique variable.
       // this means the latest event on u is always "current state"
       .observeOn(MainScheduler.instance)
       .subscribeNext { [unowned self] (u0, u1) in
@@ -54,9 +88,11 @@ class ViewController: UIViewController {
 
     let x0 : Double = 100
     
-    // y position is derived from velocity
+    // x position is derived from x-axis velocity
     u
       .asObservable()
+      // pattern demands consideration of initial state,
+      // so we inject x0 into the scan seed
       .scan(x0) { total, value in total + value * interval }
       .subscribeNext {
         self.x.value = $0
@@ -73,7 +109,7 @@ class ViewController: UIViewController {
     // initial y-axis velocity
     let v0 : Double = 0
     
-    // velocity is derived from timer
+    // v is derived from timer
     let vBase = timer
       // tick is an integer counter that increases.
       // we are ignoring it and using the interval instead
@@ -81,13 +117,14 @@ class ViewController: UIViewController {
         interval
       }
       // pattern demands consideration of initial state,
-      // so we inject it into the scan seed
+      // so we inject v0 into the scan seed
       .scan(v0) { (total, value) in
+        // here, we consider gravity, which is modeled as a constant in velocity space
         total + self.gravity * interval
     }
     
     Observable.combineLatest(vBase, vKick.asObservable()) { $0 }
-      // bind the result to a unique variable.
+      // store the result to a unique variable.
       // this means the latest event on v is always "current state"
       .observeOn(MainScheduler.instance)
       .subscribeNext { [unowned self] (v0, v1) in
@@ -99,6 +136,8 @@ class ViewController: UIViewController {
     let y0 : Double = 200
     v
       .asObservable()
+      // pattern demands consideration of initial state,
+      // so we inject y0 into the scan seed
       .scan(y0) { (total, value) in total + value * interval }
       .subscribeNext { [unowned self] in
         self.y.value = $0
